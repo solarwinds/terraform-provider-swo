66b66a090b193919f373826e32bf04a193dd2922
robstove <rob.stovenour@solarwinds.com>
2023-11-15 09:39:44 -0600

    update provider example

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -3,8 +3,19 @@ terraform {
     swo = {
       version = "0.0.5"
       source  = "solarwindscloud/swo"
-      # Uncomment the following line to use the latest version of the provider from GitHub.
-      # source = "github.com/solarwinds/swo"
+      # Uncomment the following line to use the latest version of the
+      # provider from GitHub or when using a local build.
+      # source = "github.com/solarwindscloud/swo"
     }
   }
+}
+
+provider "swo" {
+  # API token. Tokens can be created in your SWO account settings under API tokens.
+  # The token type should be Full Access.
+  api_token = "[UPDATE WITH SWO FULL ACCESS TOKEN]"
+
+  # Base URL for your SWO instance. Be sure to include your specific datacenter.
+  # Datacenter options are one of [na-01, na-02, eu-01, apj-01].
+  base_url = "https://api.na-01.cloud.solarwinds.com/v1/tfproxy"
 }
