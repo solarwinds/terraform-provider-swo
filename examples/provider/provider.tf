12f83a4d7573ae1115ba4918a8827adaf642b19f
Rob Stovenour <rob.stovenour@solarwinds.com>
2023-11-27 08:44:50 -0600

    doc updates/cleanup (#40)

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     swo = {
-      version = "0.0.5"
+      version = ">= 0.0.7"
       source  = "solarwinds/swo"
       # Uncomment the following line to use the latest version of the provider from GitHub.
       # source = "github.com/solarwinds/swo"
