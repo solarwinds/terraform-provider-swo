4c22bbc8fdf3029a728668b032427678dd85a84b
Rachel Molnar <Rachel.Molnar@solarwinds.com>
2025-09-22 12:03:12 -0700

    slightly more robust regex

--- internal/provider/alert_resource_acc_test.go
+++ internal/provider/alert_resource_acc_test.go
@@ -31,7 +31,7 @@ func TestAccEntityAlertResource(t *testing.T) {
 					// Verify the conditions.
 					resource.TestCheckResourceAttr("swo_alert.test", "conditions.0.target_entity_types.0", "Website"),
 					resource.TestCheckResourceAttr("swo_alert.test", "conditions.0.metric_name", "synthetics.https.response.time"),
-					resource.TestCheckResourceAttr("swo_alert.test", "conditions.0.threshold", "123"),
+					resource.TestCheckResourceAttr("swo_alert.test", "conditions.0.threshold", ">=12.3"),
 					resource.TestCheckResourceAttr("swo_alert.test", "conditions.0.not_reporting", "false"),
 					resource.TestCheckResourceAttr("swo_alert.test", "conditions.0.duration", "5m"),
 					resource.TestCheckResourceAttr("swo_alert.test", "conditions.0.aggregation_type", "AVG"),
@@ -317,7 +317,7 @@ resource "swo_alert" "test" {
  conditions = [
 	{
 	  metric_name      = "synthetics.https.response.time"
-	  threshold        = "123"
+	  threshold        = ">=12.3"
 	  duration         = "5m"
 	  not_reporting    = false
 	  aggregation_type = "AVG"
