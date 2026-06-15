d5db82b9c9da7ffc72f9d4638ca1fb475a109bdb
Michal Pecenka <Michal.Pecenka@solarwinds.com>
2026-06-15 13:45:53 +0200

    circleci integration

---
+++ internal/provider/circleci_integration_schema.go
@@ -0,0 +1,63 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/solarwinds/terraform-provider-swo/internal/planmodifier/stringmodifier"
+)
+
+const defaultReceiverBase = "https://webhook.swo.solarwinds.com/webhook"
+
+// circleCIIntegrationResourceModel is the main resource model.
+type circleCIIntegrationResourceModel struct {
+	Id           types.String `tfsdk:"id"`
+	Name         types.String `tfsdk:"name"`
+	ApiToken     types.String `tfsdk:"api_token"`
+	ReceiverBase types.String `tfsdk:"receiver_base"`
+	ReceiverUrl  types.String `tfsdk:"receiver_url"`
+	SecretToken  types.String `tfsdk:"secret_token"`
+}
+
+func (r *circleCIIntegrationResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description: "A terraform resource for managing CircleCI integrations.",
+		Attributes: map[string]schema.Attribute{
+			"id": resourceIdAttribute(),
+			"name": schema.StringAttribute{
+				Description: "The name of the CircleCI integration.",
+				Required:    true,
+			},
+			"api_token": schema.StringAttribute{
+				Description: "The CircleCI API token. Used to fetch project metadata and logs.",
+				Optional:    true,
+				Sensitive:   true,
+			},
+			"receiver_base": schema.StringAttribute{
+				Description: "The receiver URL base. Defaults to the SWO production endpoint.",
+				Optional:    true,
+				Computed:    true,
+				Default:     stringdefault.StaticString(defaultReceiverBase),
+			},
+			"receiver_url": schema.StringAttribute{
+				Description: "The receiver URL to configure in CircleCI project webhook.",
+				Computed:    true,
+				PlanModifiers: []planmodifier.String{
+					stringmodifier.UseNonNullStateForUnknown(),
+				},
+			},
+			"secret_token": schema.StringAttribute{
+				Description: "The secret token to configure in CircleCI project webhook and used to authenticate incoming webhooks.",
+				Computed:    true,
+				Sensitive:   true,
+				PlanModifiers: []planmodifier.String{
+					stringmodifier.UseNonNullStateForUnknown(),
+				},
+			},
+		},
+	}
+}
