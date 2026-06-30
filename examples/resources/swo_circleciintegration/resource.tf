8d66432f6618f8ebd548b3bb4309bad2c7d20955
Michal Pecenka <Michal.Pecenka@solarwinds.com>
2026-06-30 15:19:39 +0200

    name inconsistency

--- examples/resources/swo_circleciintegration/resource.tf
+++ examples/resources/swo_circleciintegration/resource.tf
@@ -1,5 +1,5 @@
 # 1. Create the SWO CircleCI integration
-resource "swo_circleci_integration" "main" {
+resource "swo_circleciintegration" "main" {
   name          = "My Organization"
   api_token     = "CIRCLECI_API_TOKEN_VALUE"                      # Optional: CircleCI API token for log aggregation
   receiver_base = "https://webhook.swo.XX.solarwinds.com/webhook" # Optional: Prefix for receiver_url. SWO endpoint where your organization is monitored.
@@ -8,8 +8,8 @@ resource "swo_circleci_integration" "main" {
 # 2. For each CircleCI project, add or remove webhook blocks as needed
 resource "circleci_webhook" "service_a" {
   name           = "SolarWinds Observability"
-  url            = swo_circleci_integration.main.receiver_url
-  signing_secret = swo_circleci_integration.main.secret_token
+  url            = swo_circleciintegration.main.receiver_url
+  signing_secret = swo_circleciintegration.main.secret_token
   scope_id       = "PROJECT_ID" # Replace with your CircleCI project UUID
   scope_type     = "project"
   events         = ["workflow-completed", "job-completed"] # SWO requires both
